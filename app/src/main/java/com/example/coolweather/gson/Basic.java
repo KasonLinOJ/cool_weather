a5787706d73090b85cd6c32baebe737eff1b58b2
linjingkai <linjingkai@meituan.com>
2020-08-07 17:18:53 +0800

    添加必应每日一图作为背景

--- app/src/main/java/com/example/coolweather/gson/Basic.java
+++ app/src/main/java/com/example/coolweather/gson/Basic.java
@@ -1,4 +1,20 @@
 package com.example.coolweather.gson;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Basic {
+    // 城市名称
+    @SerializedName("city")
+    public String cityName;
+
+    // 城市对应的天气 id
+    @SerializedName("id")
+    public String weatherId;
+    public Update update;
+
+    public class Update {
+        // 天气的更新时间
+        @SerializedName("loc")
+        public String updateTime;
+    }
 }
--- app/src/main/java/com/example/coolweather/util/HttpUtil.java
+++ app/src/main/java/com/example/coolweather/util/HttpUtil.java
@@ -1,4 +1,16 @@
 package com.example.coolweather.util;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * 封装 发起Http请求的步骤
+ */
 public class HttpUtil {
+    public static void sendOkHttpRequest(String address, okhttp3.Callback callback) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        // 传入请求地址，并注册一 个回调来处理服务器响应
+        client.newCall(request).enqueue(callback);
+    }
 }
